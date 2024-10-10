81bae18636baf4440c6f91ad33b7bb780a86079b
pharell <86664849+Pharell77coder@users.noreply.github.com>
2024-10-10 12:04:19 +0200

    Add files via upload

--- src/interactions/save.ts
+++ src/interactions/save.ts
@@ -1,14 +1,12 @@
-interface Product {
+/*interface Product {
     id: number;
     name: string;
     price: number;
     quantity: number;
   }
   
-  // Récupérer le panier depuis le localStorage
   let cart: Product[] = JSON.parse(localStorage.getItem('cart') || '[]');
   
-  // Fonction pour ajouter un produit dans le panier
   function addToCart(id: number, name: string, price: number) {
     const existingProduct = cart.find((item: Product) => item.id === id);
   
@@ -18,7 +16,6 @@ interface Product {
       cart.push({ id, name, price, quantity: 1 });
     }
   
-    // Sauvegarder le panier mis à jour dans le localStorage
     localStorage.setItem('cart', JSON.stringify(cart));
   }
-  
+  */
